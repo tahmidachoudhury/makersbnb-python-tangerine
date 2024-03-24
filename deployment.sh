ca96302fc1e967e441055c86d5b8c44fe3f34b8a
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2024-03-24 00:33:13 +0000

    render psql connection details added to deployment script

--- deployment.sh
+++ deployment.sh
@@ -19,5 +19,5 @@ echo "PostgreSQL makersbnb database seeded successfully."
 
 # Seed makersbnb_test database
 echo "Seeding PostgreSQL makersbnb database..."
-psql -h 127.0.0.1 makersbnb_test < seeds/spaces_table.sql
+PGPASSWORD=CG1HgNTJkkqN3xLs2oCQask7sI4DvF0h psql -h dpg-cnvn48i0si5c73fh0k6g-a.oregon-postgres.render.com -U makersbnb_7y56_user makersbnb_7y56 < seeds/spaces_table.sql
 echo "PostgreSQL makersbnb_test database seeded successfully."
